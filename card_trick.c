51ac51cd8ddc2c7efaa8c6ec4bd8bcfcdf6a843d
Ijinyanen <ryanhchiba@hotmail.com>
2021-01-03 00:01:52 +0000

    Solved the issue with displaying the card.Nneeded unique(j,k,l) AND initalised int in for loop. Don't know why reinitalising the same i didn't work.

--- card_trick.c
+++ card_trick.c
@@ -23,11 +23,8 @@ int main(){
 
 
     char cards[21][4] = {"AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD","AH","2H","3H","4H","5H","6H","7H","8H"};
-    for(int i = 0; i < DECK_SIZE;i++){
-        printf("%s ", cards[i]);
-    };
 
-    printf("\n welcome to the 21 card trick. V2 \n");
+    printf("\n Welcome to the 21 card trick. V2 \n");
 
     //Put cards into the main deck
     for(int i = 0; i < DECK_SIZE;i++){
@@ -39,13 +36,9 @@ int main(){
     printf("You chose %s\n", selected_card);
 
     split_deck(main_deck,sub_deck_one,sub_deck_two,sub_deck_three);
-    
-    for(int i = 0; i < SUB_DECK_SIZE; i++){
-        printf(sub_deck_one[i]);       
-    }
 
     int containing_deck_num = 0;
-    containing_deck_num = show_decks(sub_deck_one, sub_deck_two,sub_deck_three);
+    containing_deck_num = show_decks(sub_deck_one, sub_deck_two, sub_deck_three);
 
     return 0;
 }
@@ -57,7 +50,7 @@ const char* select_card(char deck[DECK_SIZE][CHAR_LENGTH], int length){
     for(int i = 0; i < length; i++){
         printf("%s ", deck[i]);
     }
-    printf("\n pick a card from the options above. \n");
+    printf("\n Pick a card from the options above. \n");
     static char chosen_card [CHAR_LENGTH];
     scanf("%s", chosen_card);
     return chosen_card;
@@ -77,21 +70,24 @@ char sub2[SUB_DECK_SIZE][CHAR_LENGTH], char sub3[SUB_DECK_SIZE][CHAR_LENGTH]){
 
 //shows the three decks
 int show_decks(char sub1[SUB_DECK_SIZE][CHAR_LENGTH], char sub2[SUB_DECK_SIZE][CHAR_LENGTH], char sub3[SUB_DECK_SIZE][CHAR_LENGTH]){
+        
     printf(" deck 1 contains: ");
-    for(int i; i < SUB_DECK_SIZE; i++){
-        printf(sub_deck_one[i]);
+    for(int j = 0; j < SUB_DECK_SIZE; j++){
+        printf("%s ", sub1[j]);
     }
+
     printf("\n");
-        printf(" deck 2 contains: ");
-    for(int i; i < SUB_DECK_SIZE; i++){
-        printf(sub2[i]);
+    printf(" deck 2 contains: ");
+    for(int k = 0; k < SUB_DECK_SIZE; k++){
+        printf("%s ",sub2[k]);
     }
+
     printf("\n");
         printf(" deck 3 contains: ");
-    for(int i; i < SUB_DECK_SIZE; i++){
-        printf(sub3[i]);
+    for(int l = 0; l < SUB_DECK_SIZE; l++){
+        printf("%s ", sub3[l]); 
     }
-    printf("\n");
+    printf("\n\n");
     
     
     int answer = 0;
